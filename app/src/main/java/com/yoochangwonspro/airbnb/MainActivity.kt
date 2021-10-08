440448e3bf78924bd3536b98ef5ab11aa805843a
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-08 17:01:28 +0900

    Feat : MapView 의 Activity LifeCycle 적용

--- app/src/main/java/com/yoochangwonspro/airbnb/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/airbnb/MainActivity.kt
@@ -13,5 +13,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    // 메모리가 별로 없을 때 호출되는 함수
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }

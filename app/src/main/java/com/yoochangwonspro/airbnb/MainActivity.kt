b4a4a452a2687435ce547b647afba90f0f492f9c
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-08 16:58:31 +0900

    Feat : MapView 전역 프로퍼티 추가

--- app/src/main/java/com/yoochangwonspro/airbnb/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/airbnb/MainActivity.kt
@@ -2,8 +2,14 @@ package com.yoochangwonspro.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapView
 
 class MainActivity : AppCompatActivity() {
+
+    private val mapView: MapView by lazy {
+        findViewById(R.id.mapView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

d15e6f056793be4ba4430c10c21327b5be34bdd7
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-13 17:41:50 +0900

    Add : HouseListAdapter class ListAdapter 상속 초기 코드 구현

--- app/src/main/java/com/yoochangwonspro/airbnb/HouseListAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/airbnb/HouseListAdapter.kt
@@ -1,4 +1,38 @@
 package com.yoochangwonspro.airbnb
 
-class HouseListAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class HouseListAdapter : ListAdapter<HouseModel, HouseListAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(houseModel: HouseModel) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<HouseModel>() {
+            override fun areItemsTheSame(oldItem: HouseModel, newItem: HouseModel): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun areContentsTheSame(oldItem: HouseModel, newItem: HouseModel): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
 }
